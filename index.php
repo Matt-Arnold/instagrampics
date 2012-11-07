14928cf92f9ca35f5698693dd1d310ba602e1a31
OpenShift guest <thematthewarnold@gmail.com>
2012-11-07 18:56:05 -0500

    HMTL created d to match home wireframs

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include('inc-head.php') ?>
+
+    <body>
+    
+        <div id="container">
+            <header>
+                <h1>Site name</h1>
+            </header>
+            <nav></nav>
+            <article>
+                <h2>Article name</h2>
+                <p>Blah blah blah blah blah blah blah Blah blah blah blah blah blah blah
+                Blah blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah 
+                blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah 
+                blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah blah 
+                blah blah blah blah Blah blah blah blah blah blah blah Blah blah blah blah blah blah blah
+                Blah blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah 
+                blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah 
+                blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah blah 
+                blah blah blah blah Blah blah blah blah blah blah blah Blah blah blah blah blah blah blah
+                Blah blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah 
+                blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah 
+                blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah blah 
+                blah blah blah blah Blah blah blah blah blah blah blah Blah blah blah blah blah blah blah
+                Blah blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah 
+                blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah 
+                blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah blah 
+                blah blah blah blah Blah blah blah blah blah blah blah Blah blah blah blah blah blah blah
+                Blah blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah 
+                blah blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah 
+                blah blah blah blah blahBlah blah blah blah blah blah blahBlah blah blah 
+                blah blah blah blah</p>
+                <iframe width="100%" height="166" scrolling="no" frameborder="no" 
+                src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F66528724&show_artwork=true"></iframe>
+            </article>
+            <aside>
+                    <iframe width="420" height="315" src="http://www.youtube.com/embed/0Bmhjf0rKe8" frameborder="0" allowfullscreen></iframe>
+                    <p>blah blahBlah blah blah blah blah blah blahBlah blah blah 
+                blah blah blah blah</p>
+            </aside>
+            <footer></footer>
+        </div>
+               
+<?php include('inc-footer.php') ?>
